26447e5ff7835ebb38e6a469426246e9a5e9e988
Marie-Louise MAUGENEST <maryse.maugenest@ensta-paris.fr>
2022-05-16 19:52:35 +0200

    finalisation de SARSA.h

--- include/SARSA.h
+++ include/SARSA.h
@@ -1,12 +1,20 @@
-#ifndef MAZEENV_H
-#define MAZEENV_H
+#ifndef SARSA_H
+#define SARSA_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-char** Q;
-float alpha;
-float epsilon;
-int iter;
+float** Q;
+float alp = 0.8;
+float eps_beginning = 0.1;
+float eps;
+float gam = 0.8;
+int iter = 100000;
+
+void MakeQ();
+
+void training();
+
+void add_crumbs();
 
 #endif
